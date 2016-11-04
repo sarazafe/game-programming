b2678dee72775475e3d28ba5b2e4d7eb6aeddf36
Sara Zapico <sarazafe@gmail.com>
2016-11-04 09:43:54 +0100

    Create lab1.cs

---
+++ week_1/labs/lab1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("My name is Sara");
+            Console.WriteLine("My best friend is Emma");
+            
+            Console.WriteLine("Sims");
+            Console.WriteLine("Mario Bross");
+            Console.WriteLine("Kirby");
+        }
+    }
+}
